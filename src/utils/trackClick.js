c40629b98ee7c3a275732dd67be3eea67a89db65
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2025-10-13 23:38:49 +0800

    click

--- src/utils/trackClick.js
+++ src/utils/trackClick.js
@@ -1,7 +1,7 @@
 // src/utils/trackClick.js
 export const trackClick = async (button, page) => {
     try {
-      await fetch("http://localhost:5000/api/clicks", {
+      await fetch("https://beyondthebrush.onrender.com/api/clicks", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ button, page }),
@@ -11,3 +11,4 @@ export const trackClick = async (button, page) => {
     }
   };
   
+ 
